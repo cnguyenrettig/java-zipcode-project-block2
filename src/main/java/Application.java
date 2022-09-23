8a1d7cf3d284940dd99f7a0f76628a9b901e9ffb
cnguyenrettig <85195704+cnguyenrettig@users.noreply.github.com>
2022-09-23 15:41:28 -0400

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
+//Zipcode Review project
 public class Application {
 
     public static void main(String[] args) {
